b3be5dd1a3382f9d560d43581cae4fdb5149f6f1
LachlanWells0001 <lachlanwells16@gmail.com>
2022-12-12 22:07:41 +0800

    Day 06 Task 1 and 2 Complete

---
+++ Day06/SignalFinder/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalFinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Filepath = @"C:\Users\VexingThoughts\source\repos\LachlanWells0001\Advent2022\Day06\SignalBuffer.txt";
+
+            String text = File.ReadAllText(Filepath);
+
+            for(int i = 0; i <= (text.Length - 3); i++) 
+            {
+                var characters = text.Substring(i, 4).Distinct();
+
+                if(characters.Count() == 4) 
+                {
+                    Console.WriteLine(i + 4);
+                    break;
+                }
+            }
+
+            //Part One Compelte
+
+            for (int i = 0; i <= (text.Length - 13); i++)
+            {
+                var characters = text.Substring(i, 14).Distinct();
+
+                if (characters.Count() == 14)
+                {
+                    Console.WriteLine(i + 14);
+                    break;
+                }
+            }
+        }
+    }
+}
